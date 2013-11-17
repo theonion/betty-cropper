73015f0ce2d521e5202014fc0a9e051bc8b41fb6
Chris Sinchok <csinchok@theonion.com>
2013-11-17 01:30:34 -0600

    Trying again for sentry

--- betty/__init__.py
+++ betty/__init__.py
@@ -2,7 +2,4 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('settings')
 
-from raven.contrib.flask import Sentry
-sentry = Sentry(app)
-
 import betty.views
--- runserver.py
+++ runserver.py
@@ -3,5 +3,8 @@ from betty import app
 from betty.database import init_db
 init_db()
 
+from raven.contrib.flask import Sentry
+sentry = Sentry(app)
+
 if __name__ == '__main__':
     app.run(debug=True)
