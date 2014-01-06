2de3806d438cf260dbd47336f689a22d5d2c5089
Chris Sinchok <csinchok@theonion.com>
2014-01-06 11:33:26 -0600

    Adding setup.py

--- betty/__init__.py
+++ betty/__init__.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask
+
+__version__ = "0.1"
 app = Flask(__name__)
 app.config.from_object(os.environ.get('SETTINGS_MODULE', 'settings'))
 
---
+++ setup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import re
+import os
+import sys
+
+
+name = 'betty-cropper'
+package = 'betty'
+description = "A flask-powered image server"
+url = "https://github.com/theonion/betty-cropper"
+author = "Chris Sinchok"
+author_email = 'csinchok@theonion.com'
+license = 'BSD'
+requires = [
+    "Flask==0.10.1",
+    "SQLAlchemy==2.2.1",
+    "psycopg2==2.5.1",
+    "slimit==0.8.1",
+    "uWSGI==1.9.19",
+    "want==0.3.5",
+]
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    args = {'version': get_version(package)}
+    print "You probably want to also tag the version now:"
+    print "  git tag -a %(version)s -m 'version %(version)s'" % args
+    print "  git push --tags"
+    sys.exit()
+
+
+setup(
+    name=name,
+    version=get_version(package),
+    url=url,
+    license=license,
+    description=description,
+    author=author,
+    author_email=author_email,
+    packages=get_packages(package),
+    package_data=get_package_data(package),
+    install_requires=requires
+)
