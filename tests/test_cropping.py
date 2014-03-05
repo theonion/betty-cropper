e5769457d4a16a777832f22080c918c4adc0b496
Chris Sinchok <csinchok@theonion.com>
2014-03-05 16:40:07 -0600

    Switching to use self.assert

--- tests/test_cropping.py
+++ tests/test_cropping.py
@@ -25,7 +25,10 @@ class ImageSavingTestCase(TestCase):
         )
 
         # Test to make sure the default selections work
-        assert image.get_selection(Ratio('1x1')) == {'x0': 0, 'y0': 0, 'x1': 512, 'y1': 512}
+        self.assertEqual(
+            image.get_selection(Ratio('1x1')),
+            {'x0': 0, 'y0': 0, 'x1': 512, 'y1': 512}
+        )
 
         # Now let's add some bad data
         image.selections = {
@@ -39,7 +42,10 @@ class ImageSavingTestCase(TestCase):
         image.save()
 
         # Now, that was a bad selection, so we should be getting an auto generated one.
-        assert image.get_selection(Ratio('1x1')) == {'x0': 0, 'y0': 0, 'x1': 512, 'y1': 512}
+        self.assertEqual(
+            image.get_selection(Ratio('1x1')),
+            {'x0': 0, 'y0': 0, 'x1': 512, 'y1': 512}
+        )
 
         # Try with a negative value
         image.selections = {
@@ -51,7 +57,10 @@ class ImageSavingTestCase(TestCase):
             }
         }
         image.save()
-        assert image.get_selection(Ratio('1x1')) == {'x0': 0, 'y0': 0, 'x1': 512, 'y1': 512}
+        self.assertEqual(
+            image.get_selection(Ratio('1x1')),
+            {'x0': 0, 'y0': 0, 'x1': 512, 'y1': 512}
+        )
 
         # Try with another negative value
         image.selections = {
@@ -63,7 +72,10 @@ class ImageSavingTestCase(TestCase):
             }
         }
         image.save()
-        assert image.get_selection(Ratio('1x1')) == {'x0': 0, 'y0': 0, 'x1': 512, 'y1': 512}
+        self.assertEqual(
+            image.get_selection(Ratio('1x1')),
+            {'x0': 0, 'y0': 0, 'x1': 512, 'y1': 512}
+        )
 
         # Try with bad x values
         image.selections = {
@@ -75,23 +87,26 @@ class ImageSavingTestCase(TestCase):
             }
         }
         image.save()
-        assert image.get_selection(Ratio('1x1')) == {'x0': 0, 'y0': 0, 'x1': 512, 'y1': 512}
+        self.assertEqual(
+            image.get_selection(Ratio('1x1')),
+            {'x0': 0, 'y0': 0, 'x1': 512, 'y1': 512}
+        )
 
     def test_bad_image_id(self):
         res = self.client.get('/images/abc/13x4/256.jpg')
-        assert res.status_code == 404
+        self.assertEqual(res.status_code, 404)
 
     def test_bad_ratio(self):
         res = self.client.get('/images/666/13x4/256.jpg')
-        assert res.status_code == 404
+        self.assertEqual(res.status_code, 404)
 
     def test_bad_extension(self):
         res = self.client.get('/images/666/1x1/500.gif')
-        assert res.status_code == 404
+        self.assertEqual(res.status_code, 404)
 
     def test_too_large(self):
         res = self.client.get('/images/666/1x1/2001.jpg')
-        assert res.status_code == 500
+        self.assertEqual(res.status_code, 500)
 
     def test_image_redirect(self):
         res = self.client.get('/images/66666/1x1/100.jpg')
@@ -101,20 +116,20 @@ class ImageSavingTestCase(TestCase):
         settings.BETTY_PLACEHOLDER = True
 
         res = self.client.get('/images/666/original/256.jpg')
-        assert res['Content-Type'] == 'image/jpeg'
-        assert res.status_code == 200
+        self.assertEqual(res['Content-Type'], 'image/jpeg')
+        self.assertEqual(res.status_code, 200)
 
         res = self.client.get('/images/666/1x1/256.jpg')
-        assert res.status_code == 200
-        assert res['Content-Type'] == 'image/jpeg'
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res['Content-Type'], 'image/jpeg')
 
         res = self.client.get('/images/666/1x1/256.png')
-        assert res['Content-Type'] == 'image/png'
-        assert res.status_code == 200
+        self.assertEqual(res['Content-Type'], 'image/png')
+        self.assertEqual(res.status_code, 200)
 
         settings.BETTY_PLACEHOLDER = False
         res = self.client.get('/images/666/1x1/256.jpg')
-        assert res.status_code == 404
+        self.assertEqual(res.status_code, 404)
 
     def test_missing_file(self):
         image = Image.objects.create(name="Lenna.gif", width=512, height=512)
@@ -134,21 +149,21 @@ class ImageSavingTestCase(TestCase):
 
         # Now let's test that a JPEG crop will return properly.
         res = self.client.get('/images/%s/1x1/240.jpg' % image.id)
-        assert res['Content-Type'] == 'image/jpeg'
-        assert res.status_code == 200
-        assert os.path.exists(os.path.join(image.path(), '1x1', '240.jpg'))
+        self.assertEqual(res['Content-Type'], 'image/jpeg')
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(os.path.exists(os.path.join(image.path(), '1x1', '240.jpg')))
 
         # Now let's test that a PNG crop will return properly.
         res = self.client.get('/images/%s/1x1/240.png' % image.id)
-        assert res['Content-Type'] == 'image/png'
-        assert res.status_code == 200
-        assert os.path.exists(os.path.join(image.path(), '1x1', '240.png'))
+        self.assertEqual(res['Content-Type'], 'image/png')
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(os.path.exists(os.path.join(image.path(), '1x1', '240.png')))
 
         # Let's test an "original" crop
         res = self.client.get('/images/%s/original/240.jpg' % image.id)
-        assert res['Content-Type'] == 'image/jpeg'
-        assert res.status_code == 200
-        assert os.path.exists(os.path.join(image.path(), 'original', '240.jpg'))
+        self.assertEqual(res['Content-Type'], 'image/jpeg')
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(os.path.exists(os.path.join(image.path(), 'original', '240.jpg')))
 
         # Finally, let's test a width that doesn't exist
         res = self.client.get('/images/%s/original/666.jpg' % image.id)
