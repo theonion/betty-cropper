39502f300f3086d0f3f4e208d26f211c6d11e193
Chris Sinchok <csinchok@theonion.com>
2014-03-05 13:04:02 -0600

    Fixing upload

--- betty/server/urls.py
+++ betty/server/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^upload\.html', 'betty.server.views.upload'),  # noqa
-    url(r'^api/new$', 'betty.server.api.new'),
+    url(r'^api/new$', 'betty.server.api.new'),  # noqa
     url(r'^api/search$', 'betty.server.api.search'),
     url(r'^api/(?P<image_id>\d+)/(?P<ratio_slug>[a-z0-9]+)$', 'betty.server.api.update_selection'),
     url(r'^api/(?P<image_id>\d+)$', 'betty.server.api.detail'),
