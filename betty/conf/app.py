ff3e0026c982fa1822841380ba178370e133e5b7
Chris Sinchok <csinchok@theonion.com>
2014-03-06 16:13:34 -0600

    I guess python3 doesn't have urlparse

--- betty/conf/app.py
+++ betty/conf/app.py
@@ -2,13 +2,16 @@
 from __future__ import unicode_literals
 
 import os
-import urlparse
+try:
+    from urlparse import urljoin
+except ImportError:
+    from urllib.parse import urljoin
 
 from django.conf import settings as _settings
 
 DEFAULTS = {
     "BETTY_IMAGE_ROOT": os.path.join(_settings.MEDIA_ROOT, "images"),
-    "BETTY_IMAGE_URL": urlparse.urljoin(_settings.MEDIA_URL, "images/"),
+    "BETTY_IMAGE_URL": urljoin(_settings.MEDIA_URL, "images/"),
     "BETTY_RATIOS": ("1x1", "2x1", "3x1", "3x4", "4x3", "16x9"),
     "BETTY_WIDTHS": (80, 150, 240, 300, 320, 400, 480, 620, 640, 820, 960, 1200, 1600),
     "BETTY_PLACEHOLDER": _settings.DEBUG,
