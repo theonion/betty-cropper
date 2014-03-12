41245fc34f61a69a56f4815f82d86a7423cd4601
Chris Sinchok <csinchok@theonion.com>
2014-03-12 12:44:15 -0500

    Fixing tests on 1.4, I hope

--- betty/client/fields.py
+++ betty/client/fields.py
@@ -1,4 +1,11 @@
+import math
+
+from django.core import exceptions
 from django.db.models.fields.files import FieldFile, FileField
+from django.utils.translation import ugettext_lazy as _
+
+
+from betty.client.storage import BettyCropperStorage
 
 
 class ImageFieldFile(FieldFile):
@@ -6,4 +13,45 @@ class ImageFieldFile(FieldFile):
 
 
 class ImageField(FileField):
-    pass
+
+    attr_class = ImageFieldFile
+
+    def __init__(self, verbose_name=None, name=None, storage=BettyCropperStorage, **kwargs):
+
+        super(self, ImageField).__init__(
+            verbose_name=verbose_name,
+            name=name,
+            storage=storage,
+            **kwargs)
+
+    empty_strings_allowed = False
+    default_error_messages = {
+        'invalid': _("'%(value)s' value must be an integer."),
+    }
+
+    def to_python(self, value):
+        if value is None:
+            return value
+        try:
+            return str(value)
+        except (TypeError, ValueError):
+            raise exceptions.ValidationError(
+                self.error_messages['invalid'],
+                code='invalid',
+                params={'value': value},
+            )
+
+    def get_prep_value(self, value):
+        value = super(ImageField, self).get_prep_value(value)
+        if value is None:
+            return None
+        return int(value)
+
+    def get_prep_lookup(self, lookup_type, value):
+        if ((lookup_type == 'gte' or lookup_type == 'lt')
+                and isinstance(value, float)):
+            value = math.ceil(value)
+        return super(ImageField, self).get_prep_lookup(lookup_type, value)
+
+    def get_internal_type(self):
+        return "IntegerField"
--- tests/test_management_commands.py
+++ tests/test_management_commands.py
@@ -8,12 +8,12 @@ from betty.server.auth import ApiToken
 class CreateTokenTestCase(TestCase):
 
     def test_create_token(self):
+        token_count = ApiToken.objects.count()
         management.call_command("create_token")
-        self.assertEqual(ApiToken.objects.count(), 1)
+        self.assertEqual(ApiToken.objects.count(), token_count + 1)
 
     def test_create_specific_token(self):
         management.call_command("create_token", "noop", "noop")
-        self.assertEqual(ApiToken.objects.count(), 1)
         qs = ApiToken.objects.filter(private_token="noop", public_token="noop")
         self.assertEqual(qs.count(), 1)
 
