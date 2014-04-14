78ca854f6649d467580fb3d9964905ae5ccac5eb
Chris Sinchok <csinchok@theonion.com>
2014-04-14 17:05:49 -0500

    Working on converting placeholder

--- betty/cropper/utils/placeholder.py
+++ betty/cropper/utils/placeholder.py
@@ -1,8 +1,7 @@
+import io
 import random
 
-from wand.color import Color
-from wand.drawing import Drawing
-from wand.image import Image
+from PIL import Image, ImageDraw, ImageFont
 
 from betty.cropper.models import Ratio
 from betty.conf.app import settings
@@ -11,19 +10,27 @@ from betty.conf.app import settings
 def placeholder(ratio, width, extension):
     if ratio.string == "original":
         ratio = Ratio(random.choice((settings.BETTY_RATIOS)))
-    height = (width * ratio.height / float(ratio.width))
-    with Drawing() as draw:
-        draw.font_size = 52
-        draw.gravity = "center"
-        draw.fill_color = Color("white")
-        with Color(random.choice(settings.BETTY_PLACEHOLDER_COLORS)) as bg:
-            with Image(width=width, height=int(height), background=bg) as img:
-                draw.text(0, 0, ratio.string)
-                draw(img)
+    height = int(round((width * ratio.height / float(ratio.width))))
 
-                if extension == 'jpg':
-                    img.format = 'jpeg'
-                if extension == 'png':
-                    img.format = 'png'
+    img = Image.new("RGB", (width, height))
 
-                return img.make_blob()
+    draw = ImageDraw.Draw(img)
+    bg_fill = random.choice(settings.BETTY_PLACEHOLDER_COLORS)
+    draw.rectangle((0, 0, width, height), fill=bg_fill)
+
+    print(settings.BETTY_PLACEHOLDER_FONT)
+    font = ImageFont.truetype(filename=settings.BETTY_PLACEHOLDER_FONT, size=45)
+    text_size = draw.textsize(ratio.string, font=font)
+    text_coords = (
+        int(round((width / 2.0) - (text_size[0] / 2.0))),
+        int(round((height / 2.0) - (text_size[1] / 2.0))),
+    )
+    draw.text(text_coords, ratio.string, font=font)
+    if extension == 'jpg':
+        pillow_kwargs = {"format": "jpeg", "quality": 80}
+    if extension == 'png':
+        pillow_kwargs = {"format": "png"}
+
+    tmp = io.BytesIO()
+    img.save(tmp, **pillow_kwargs)
+    return tmp.getvalue()
