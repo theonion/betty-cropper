b2783e28e950e70a25abc88b703385449fc4c5f3
Chris Sinchok <csinchok@theonion.com>
2014-12-03 15:18:04 -0600

    Adding migration

---
+++ betty/cropper/migrations/0002_auto_20141203_2115.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import betty.cropper.models
+import django.core.files.storage
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cropper', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='image',
+            name='optimized',
+            field=models.FileField(storage=django.core.files.storage.FileSystemStorage(base_url=b'/', location=b'/Users/csinchok/Development/betty-cropper/images'), max_length=255, null=True, upload_to=betty.cropper.models.optimized_upload_to, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='image',
+            name='source',
+            field=models.FileField(storage=django.core.files.storage.FileSystemStorage(base_url=b'/', location=b'/Users/csinchok/Development/betty-cropper/images'), max_length=255, null=True, upload_to=betty.cropper.models.source_upload_to, blank=True),
+            preserve_default=True,
+        ),
+    ]
