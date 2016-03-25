913c9342ab6bbfbf1ef5587a9958d1aeae551a74
Mike Parent <mparent@umich.edu>
2016-03-25 12:13:32 -0500

    Even better alternate storage test
    Because I love refactoring. Verifies correct image data saved.

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -17,19 +17,24 @@ def test_alternate_storage(admin_client, settings):
 
     storage = InMemoryStorage()
     with patch('django.db.models.fields.files.default_storage._wrapped', storage):
-        # Create Image
-        path = os.path.join(TEST_DATA_PATH, 'animated.gif')
-        with open(path, "rb") as image:
-            resp = admin_client.post('/images/api/new', {"image": image})
-        assert resp.status_code == 200
-        image_id = json.loads(resp.content.decode("utf-8"))['id']
 
-        assert storage.filesystem.exists('images/{}/animated.gif'.format(image_id))
-        assert storage.filesystem.exists('images/{}/optimized.gif'.format(image_id))
+        # Create Image
+        path = os.path.join(TEST_DATA_PATH, 'lenna.png')
+        with open(path, "rb") as image:
+
+            resp = admin_client.post('/images/api/new', {"image": image})
+            assert resp.status_code == 200
+            image_id = json.loads(resp.content.decode("utf-8"))['id']
+
+            image.seek(0)
+            image_data = image.read()
+            storage_data = storage.filesystem.open('images/{}/lenna.png'.format(image_id)).read()
+            assert image_data == storage_data
+            assert storage.filesystem.exists('images/{}/optimized.png'.format(image_id))
 
         # Delete Image
         resp = admin_client.post("/images/api/{0}".format(image_id),
                                  REQUEST_METHOD="DELETE")
 
-        assert not storage.filesystem.exists('images/{}/animated.gif'.format(image_id))
-        assert not storage.filesystem.exists('images/{}/optimized.gif'.format(image_id))
+        assert not storage.filesystem.exists('images/{}/lenna.png'.format(image_id))
+        assert not storage.filesystem.exists('images/{}/optimized.png'.format(image_id))
